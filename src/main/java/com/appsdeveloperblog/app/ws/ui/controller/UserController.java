e67bd144aa89152a8d1bd496c27ef9fd04048d09
Aks32216 <amishsingh568@gmail.com>
2024-05-04 16:25:02 +0530

    sending different format response

--- src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.appsdeveloperblog.app.ws.ui.controller;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,18 +10,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.appsdeveloperblog.app.ws.ui.model.response.UserRest;
+
 @RestController
 @RequestMapping("users")
 public class UserController {
 	
 	@GetMapping
-	public String getUser(@RequestParam(value="page") int page, @RequestParam(value="limit") int limit) {
+	public String getUser(@RequestParam(value="page", defaultValue="1") int page, @RequestParam(value="limit") int limit) {
 		return "get user was called for page and limit" + page+ " "+ limit;
 	}
 	
-	@GetMapping(path="/{userId}")
-	public String getUser(@PathVariable String userId) {
-		return "get user was called for user : "+userId;
+	@GetMapping(path="/{userId}", produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+	public UserRest getUser(@PathVariable String userId) {
+		
+		UserRest returnValue=new UserRest();
+		returnValue.setEmail("amishsingh568@gmail.com");
+		returnValue.setFirstName("Amish");
+		returnValue.setLastName("Singh");
+		
+		return returnValue;
 	}
 	
 	@PostMapping
